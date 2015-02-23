803fc42bb50b63648019345e09573208e05f2dbc
csrobinson86 <csrobinson86@gmail.com>
2015-02-23 08:18:58 -0500

    primitive types and ref types

---
+++ primitive_types_and_reference_types.js
@@ -0,0 +1,17 @@
+var a = 3.14; // Declare and initialize a variable
+var b = a; // Copy the variable's value to a new variable
+a = 4; // Modify the value of the original variable
+alert(b) // Displays 3.14; the copy has not changed
+
+var a = [1,2,3]; // Initialize a variable to refer to an array
+var b = a; // Copy that reference into a new variable
+a[0] = 99; // Modify the array using original reference
+aler(b); // Display the changed array [99,2,3] using the new reference
+
+/*
+
+Primitive Types: numbers, boolean values, null, undefined
+
+Reference Types: objects, arrays, and functions
+
+*/
